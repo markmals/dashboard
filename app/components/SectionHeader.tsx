a607884de26145eb6ff4a1c4d7a5568ac446c251
Mark Malstrom <mark@malstrom.me>
2024-09-21 21:11:28 -0500

    Fix props type mismatch

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -1,6 +1,7 @@
 import { Divider, Heading } from "@tailwindcss/ui"
+import { PropsWithChildren } from "react"
 
-export function SectionHeader({ children, className }: { children: string; className?: string }) {
+export function SectionHeader({ children, className }: { className?: string } & PropsWithChildren) {
     return (
         <div className={className}>
             <Heading className="mb-4 truncate">{children}</Heading>
