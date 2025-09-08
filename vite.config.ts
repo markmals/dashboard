120cffecd75b0ad3b12d8d87ca6ffc63c4690f42
Mark Malstrom <mark@malstrom.me>
2025-09-08 01:42:13 -0500

    Update React Compiler plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { reactCompiler } from "react-compiler.plugin";
 import { defineConfig } from "vite";
-import babel from "vite-plugin-babel";
 
 export default defineConfig({
     plugins: [
         cloudflare({ viteEnvironment: { name: "ssr" } }),
         reactRouter(),
-        babel({
-            filter: /\.[jt]sx?$/,
-            babelConfig: {
-                presets: ["@babel/preset-typescript"],
-                plugins: [["babel-plugin-react-compiler", {}]],
-            },
-        }),
+        reactCompiler(),
         tailwindcss(),
     ],
     experimental: {
