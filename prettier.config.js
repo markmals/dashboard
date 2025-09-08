febb85af023d446b138816fefad2187ff8e08608
Mark Malstrom <mark@malstrom.me>
2025-09-08 01:08:34 -0500

    Convert to Prettier

---
+++ prettier.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+export default {
+    printWidth: 100,
+    tabWidth: 4,
+    arrowParens: "avoid",
+
+    // MARK: Defaults
+    // useTabs: false,
+    // semi: true,
+    // singleQuote: false,
+    // trailingComma: "all",
+    // proseWrap: "preserve",
+
+    plugins: [
+        "@prettier/plugin-oxc",
+        "prettier-plugin-pkg",
+        "prettier-plugin-sh",
+        "prettier-plugin-tailwindcss",
+        "prettier-plugin-toml",
+    ],
+
+    overrides: [
+        {
+            files: ["*.jsonc"],
+            options: {
+                trailingComma: "none",
+            },
+        },
+    ],
+};
